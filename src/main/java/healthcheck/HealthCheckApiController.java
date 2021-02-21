356adc34e32d90fcd56da530d86c0e89b679eeab
sangwoo-98 <robbyra@gmail.com>
2021-02-21 13:44:27 +0900

    Add GetMapping to /v1/health-check

--- src/main/java/healthcheck/HealthCheckApiController.java
+++ src/main/java/healthcheck/HealthCheckApiController.java
@@ -1,12 +1,14 @@
 package healthcheck;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckApiController {
 
+    @GetMapping("/v1/health-check")
     @ResponseStatus(HttpStatus.OK)
     public String returnHealthy() {
         return "I AM HEALTHY!";
