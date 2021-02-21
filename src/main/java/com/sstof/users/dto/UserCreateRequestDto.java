d7f8385106165396c15b6d787c16efd33072ac2f
sangwoo-98 <robbyra@gmail.com>
2021-02-21 15:21:17 +0900

    Add UserCreateRequestDto

---
+++ src/main/java/com/sstof/users/dto/UserCreateRequestDto.java
@@ -0,0 +1,24 @@
+package com.sstof.users.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@NoArgsConstructor
+@Getter
+public class UserCreateRequestDto {
+
+    @NotBlank(message = "Name field is required.")
+    private String name;
+
+    @Email
+    @NotBlank(message = "Email field is required.")
+    private String email;
+
+    @NotBlank(message = "Password field is required.")
+    @Length(min = 64, max = 64)
+    private String password;
+}
