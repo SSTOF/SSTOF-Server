3c0134ba3b9d258a80eeabfc728207b4bec18e32
sangwoo-98 <robbyra@gmail.com>
2021-02-27 15:20:54 +0900

    Add security config

---
+++ src/main/java/com/sstof/config/auth/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.sstof.config.auth;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(a -> a
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated())
+                .exceptionHandling(e -> e
+                    .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(githubOAuth2UserService);
+    }
+}
