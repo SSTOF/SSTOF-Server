19fb64d95a058269cc2c86404ecd7750b223504b
sangwoo-98 <robbyra@gmail.com>
2021-02-27 15:24:05 +0900

    Add GithubOAuth2UserService

---
+++ src/main/java/com/sstof/config/auth/GithubOAuth2UserService.java
@@ -0,0 +1,30 @@
+package com.sstof.config.auth;
+
+import com.sstof.users.domain.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class GithubOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    private final UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        System.out.println(registrationId);
+        String userNameAttributeName = userRequest.getClientRegistration()
+                .getProviderDetails().getUserInfoEndpoint()
+                .getUserNameAttributeName();
+        System.out.println(userNameAttributeName);
+        // TODO
+    }
+}
--- src/main/java/com/sstof/config/auth/SecurityConfig.java
+++ src/main/java/com/sstof/config/auth/SecurityConfig.java
@@ -1,13 +1,18 @@
 package com.sstof.config.auth;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final GithubOAuth2UserService githubOAuth2UserService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
