8a78c4bef2b173e0e872a045d5c121a5c5b64a46
sangwoo-98 <robbyra@gmail.com>
2021-03-01 18:58:20 +0900

    Disable Spring security features since it is Rest API

--- src/main/java/com/sstof/config/auth/SecurityConfig.java
+++ src/main/java/com/sstof/config/auth/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.sstof.config.auth;
 
+import com.sstof.users.domain.UserRole;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @RequiredArgsConstructor
@@ -16,13 +18,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests(a -> a
-                .antMatchers("/").permitAll()
-                .anyRequest().authenticated())
-                .exceptionHandling(e -> e
-                    .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
-                .oauth2Login()
-                .userInfoEndpoint()
-                .userService(githubOAuth2UserService);
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/v1/**")
+                .permitAll();
     }
 }
--- src/main/java/com/sstof/users/domain/UserRole.java
+++ src/main/java/com/sstof/users/domain/UserRole.java
@@ -1,5 +1,13 @@
 package com.sstof.users.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum UserRole {
-    USER, ADMIN
+    USER("ROLE_USER", "user"),
+    ADMIN("ROLE_ADMIN", "admin");
+    private final String key;
+    private final String title;
 }
