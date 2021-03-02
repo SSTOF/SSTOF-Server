cc94ed8134c8c04d23a6340a9def238a06355b15
sangwoo-98 <robbyra@gmail.com>
2021-03-02 17:59:09 +0900

    GithubEmailNotPublicException extends org.springframework.security.core.AuthenticationException

--- src/main/java/com/sstof/auth/exception/GithubEmailNotPublicException.java
+++ src/main/java/com/sstof/auth/exception/GithubEmailNotPublicException.java
@@ -1,14 +1,12 @@
 package com.sstof.auth.exception;
 
-import com.sstof.common.exception.BadRequestException;
-import org.springframework.security.oauth2.core.OAuth2AuthorizationException;
-import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.core.AuthenticationException;
 
-public class GithubEmailNotPublicException extends OAuth2AuthorizationException {
+public class GithubEmailNotPublicException extends AuthenticationException {
 
     private static final String MESSAGE = "깃허브 링크에 Public Email이 없습니다. 설정 후 다시 시도해 주세요.";
 
     public GithubEmailNotPublicException() {
-        super(new OAuth2Error(MESSAGE));
+        super(MESSAGE);
     }
 }
