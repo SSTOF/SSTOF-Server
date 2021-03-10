9c632d6e4b38df8d937cf219f5342a990d3c3e0c
sangwoo-98 <robbyra@gmail.com>
2021-03-10 20:55:13 +0900

    Send error_code as query parameter if error occurs

--- src/main/java/com/sstof/common/global/GithubOAuthExceptionHandler.java
+++ src/main/java/com/sstof/common/global/GithubOAuthExceptionHandler.java
@@ -17,23 +17,16 @@ import java.util.Map;
 @NoArgsConstructor
 public class GithubOAuthExceptionHandler implements AuthenticationFailureHandler {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        System.out.println("HANDLE!");
         if(exception instanceof GithubException) {
             GithubException githubException =  (GithubException) exception;
             response.setStatus(githubException.getStatus());
             Map<String, Object> data = new HashMap<>();
-            data.put("timestamp", LocalDateTime.now());
-            data.put("message", githubException.getMessage());
-            data.put("exception", githubException.getMessage());
 
             response.setContentType("application/json");
             response.setCharacterEncoding("UTF8");
-
-            response.getWriter().println(objectMapper.writeValueAsString(data));
+            response.sendRedirect("http://localhost:3000/signup?error_code=" + githubException.getStatus());
         }
     }
 }
