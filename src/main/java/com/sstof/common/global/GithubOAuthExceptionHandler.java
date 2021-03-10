ceef3683833c70d2bf32430bfb869e1e3b58c0b4
sangwoo-98 <robbyra@gmail.com>
2021-03-10 20:55:37 +0900

    Remove unused imports

--- src/main/java/com/sstof/common/global/GithubOAuthExceptionHandler.java
+++ src/main/java/com/sstof/common/global/GithubOAuthExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.sstof.common.global;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sstof.auth.exception.GithubException;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.AuthenticationException;
@@ -10,7 +9,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,7 +20,6 @@ public class GithubOAuthExceptionHandler implements AuthenticationFailureHandler
         if(exception instanceof GithubException) {
             GithubException githubException =  (GithubException) exception;
             response.setStatus(githubException.getStatus());
-            Map<String, Object> data = new HashMap<>();
 
             response.setContentType("application/json");
             response.setCharacterEncoding("UTF8");
